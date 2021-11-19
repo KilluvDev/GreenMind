8fd44e8c5eae48ed67f751edd34b0813cb1c35f1
Ignacio Trujillo <ignaciotrujillo@MacBook-Air-de-Ignacio.local>
2021-11-19 17:11:10 -0300

    Second version

--- GreenMind/urls.py
+++ GreenMind/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from GreenMind import views as local_views
 
+# path('Green-mind/',local_views.),
+
 urlpatterns = [
-    path('GreenMind/', local_views.home),
+    path('Green-mind/', local_views.home),
+    path('Green-mind/Calcula-tu-huella/',local_views.form),
+    path('Green-mind/User-account/',local_views.useracc),
+    path('Green-mind/History-of-use/',local_views.history),
 ]
--- GreenMind/views.py
+++ GreenMind/views.py
@@ -12,6 +12,13 @@ import json
 #import pdb; pdb.set_trace()   # Debuger
 
 def home(request):
-	
-	return render(request,'/index.html')
+	return render(request,'index.html',{})
 
+def form(request):
+	return render(request,'forms.html',{})
+
+def history(request):
+	return render(request,'history.html',{})
+
+def useracc(request):
+	return render(request,'user.html',{})
